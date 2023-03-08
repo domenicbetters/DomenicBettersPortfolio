53333dc923f7c2ff99cd6731c9bc0d852c2ebf6a
Domenic Betters <108141041+domenicbetters@users.noreply.github.com>
2023-03-08 12:36:09 -0500

    added main components of main page

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,7 +1,19 @@
 <template>
     <div>
-      <div style="width: 95vw; height: 1600px; border: 1px solid pink;">
-      <h1>The Portfolio of One Domenic Betters</h1></div>
+        <div class="main-page">
+            <div class="main-page-left">
+                <h1>The Portfolio of One Domenic Betters</h1>
+                <div class="menu-box">
+                    <router-link to="/"><p>Projects</p></router-link>
+                    <router-link to="/"><p>Resume</p></router-link>
+                    <router-link to="/"><p>About</p></router-link>
+                    <router-link to="/"><p>Contact</p></router-link>
+                </div>
+            </div>
+            <div class="main-page-right">
+                
+            </div>
+        </div>   
     </div>
 </template>
   
@@ -18,9 +30,19 @@
   
   <style scoped>
   h1 {
-    width: 50%;
     font-family: "nightside";
     font-size: 7em;
+  }
+  
+  a {
+    text-decoration: none;
+  }
+
+  p {
+    border-bottom: 4px solid  rgb(57, 3, 63);;
+  border-radius: 255px 15px 225px 15px/15px 225px 15px 255px;
+  margin: 60px;
+
   }
   </style>
   
