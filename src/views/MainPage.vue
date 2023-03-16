4c008407e5d0cd675cab6bc11b58a2f68008d9e6
Domenic Betters <108141041+domenicbetters@users.noreply.github.com>
2023-03-16 18:31:44 -0400

    fixed broken link

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -4,16 +4,16 @@
             <div class="main-page-left">
                 <h1 class=" fade-in">The Portfolio of One Domenic Betters</h1>
                 <div class="menu-box fourk  fade-in-delay-5 ">
-                    <router-link to="/dbettersport/projects"><p class = " slide-bottom-delay-5 menu-link">Projects</p></router-link>
-                    <router-link to="/dbettersport/resume"><p class = " slide-bottom-delay-1 menu-link">Resume</p></router-link>
+                    <router-link to="/DomenicBettersPortfolio/projects"><p class = " slide-bottom-delay-5 menu-link">Projects</p></router-link>
+                    <router-link to="/DomenicBettersPortfolio/resume"><p class = " slide-bottom-delay-1 menu-link">Resume</p></router-link>
                     <router-link to="/"><p class = " slide-bottom-delay-15 menu-link">About</p></router-link>
                     <router-link to="/"><p class = " slide-bottom-delay-2 menu-link">Contact</p></router-link>
                 </div>
             </div>
             <div class="main-page-right fade-in-delay-2s">
               <div class="menu-box menu-mobile ten80  fade-in-delay-5t ">
-                    <router-link to="/dbettersport/projects"><p class = " slide-bottom-delay-5t menu-link">Projects</p></router-link>
-                    <router-link to="/dbettersport/resume"><p class = " slide-bottom-delay-1t menu-link">Resume</p></router-link>
+                    <router-link to="/DomenicBettersPortfolio/projects"><p class = " slide-bottom-delay-5t menu-link">Projects</p></router-link>
+                    <router-link to="/DomenicBettersPortfolio/resume"><p class = " slide-bottom-delay-1t menu-link">Resume</p></router-link>
                     <router-link to="/"><p class = " slide-bottom-delay-15t menu-link">About</p></router-link>
                     <router-link to="/"><p class = " slide-bottom-delay-2t menu-link">Contact</p></router-link>
                 </div>
