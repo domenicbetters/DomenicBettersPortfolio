6b216809b6fe4ae71951df195bc73ad4cb2a97dd
Domenic Betters <108141041+domenicbetters@users.noreply.github.com>
2023-03-18 13:26:32 -0400

    further book formatting fixes.  Will need to adjust for mobile and scalable text font next.

--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -2,37 +2,35 @@
   <div class = "project-body">
    <router-link to="/"><p class = "back-button menu-link">Home</p></router-link>
    <h1>The Projects of one Domenic Betters</h1>
-
-    <div id="book">
-      <div class="my-page" data-density="soft">
-          <h2>Gluttony of Gore</h2>
-          <div class = "project-img" >
-            <iframe width="100%" height="415" src="https://www.youtube.com/embed/uBbYOAs8s50?autoplay=1&mute=1&controls=0&loop=1&playlist=uBbYOAs8s50" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; loop; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          </div>
+    <div class="book-box">
+      <div id="book">
+        <div class="my-page" data-density="soft">
+            <h2>Gluttony of Gore</h2>
+            <div class = "project-img" >
+              <iframe width="100%" height="100%" src="https://www.youtube.com/embed/uBbYOAs8s50?autoplay=1&mute=1&controls=0&loop=1&playlist=uBbYOAs8s50" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; loop; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="my-page info-page">
+            <h3>Gluttony of Gore (continued...)</h3>
+            <h4>What is it?</h4>
+            <p>Glutton of Gore is a ruby project using the ruby2d gem.  This gem allows for basic graphic interfaces and sprite animations.  Using this as a base, "Gluttony of Gore" was written from the ground up.  Everything from gravity and jump phsyics to hit-box detection was coded from scratch.  The artwork in the finished project will all be original work by myself as well.  Placeholders from the game "Splatterhouse" are being used in the mean time.  </p>
+            
+        </div>
+        <div class="my-page">
+            Page two
+        </div>
+        <div class="my-page">
+            Page three
+        </div>
+        <div class="my-page">
+            Page four
+        </div>
+        <div class="my-page" data-density="soft">
+            Last page
+        </div>
       </div>
-      <div class="my-page info-page">
-          <h3>Gluttony of Gore (continued...)</h3>
-          <h4>What is it?</h4>
-          <p>Glutton of Gore is a ruby project using the ruby2d gem.  This gem allows for basic graphic interfaces and sprite animations.  Using this as a base, "Gluttony of Gore" was written from the ground up.  Everything from gravity and jump phsyics to hit-box detection was coded from scratch.  The artwork in the finished project will all be original work by myself as well.  Placeholders from the game "Splatterhouse" are being used in the mean time.  </p>
-          
-      </div>
-      <div class="my-page">
-          Page two
-      </div>
-      <div class="my-page">
-          Page three
-      </div>
-      <div class="my-page">
-          Page four
-      </div>
-      <div class="my-page" data-density="soft">
-          Last page
-      </div>
-</div>
-
-
-
-</div>
+    </div>
+  </div>
 </template>
 
 
@@ -55,10 +53,10 @@ async mounted() {
 width: 600, // required parameter - base page width
 height: 800, // required parameter - base page height
 size: "stretch",
-minWidth: 215,
-maxWidth: 1000,
-minHeight: 420,
-maxHeight: 1350,
+minWidth: 200,
+maxWidth: 1200,
+minHeight: 266.666,
+maxHeight: 1600,
 
 });
 
@@ -77,12 +75,13 @@ maxHeight: 1350,
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
   }
 
 #book {
-  margin: 50px;
   box-shadow: #494029 5px 0px,  #494029 -5px 0px, #6f6446 10px 0px,  #6f6446 -10px 0px, #988c6c 15px 0px, #988c6c -15px 0px, #b4a98b -20px 0px, #b4a98b 20px 0px, #dbd2ba -25px 0px, #dbd2ba 25px 0px,#1d170a 30px 0px, #1d170a -30px 0px;
   border-radius: 1px;
+  overflow: hidden;
 }
 
 
@@ -103,10 +102,25 @@ a {
     text-decoration: none;
   }
 
+  .book-box {
+    width:100%; 
+    max-width: 75vw; 
+    margin:50px;
+    padding-left:30px;
+    padding-right:30px;
+    box-shadow: 0 6px 15px 7px rgba(0, 0, 0, 0.664);
+  }
+
 .project-img {
   width: 75%;
+  height: 415px;
+  max-height: 70%;
   overflow: hidden;
   border-radius: 255px 15px 225px 15px/15px 225px 15px 255px;
+}
+
+.project-img iframe {
+  transform: scale(2);
 }
 img {
   width: 100%;
