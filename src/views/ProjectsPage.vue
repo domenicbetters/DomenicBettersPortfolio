83da85a23f900371961df02d2af72aea0bf0fd32
Domenic Betters <108141041+domenicbetters@users.noreply.github.com>
2023-04-07 08:58:06 -0400

    further work on project content and book functionality

--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -14,6 +14,13 @@
             <h3>Gluttony of Gore (continued...)</h3>
             <h4>What is it?</h4>
             <p>Glutton of Gore is a ruby project using the ruby2d gem.  This gem allows for basic graphic interfaces and sprite animations.  Using this as a base, "Gluttony of Gore" was written from the ground up.  Everything from gravity and jump phsyics to hit-box detection was coded from scratch.  The artwork in the finished project will all be original work by myself as well.  Placeholders from the game "Splatterhouse" are being used in the mean time.  </p>
+
+            <h4>What did I learn?</h4>
+            <p>Given that ruby 2d is a simple graphical framework with no bells or whistles, I had to learn a surprising amount about physics and game mechanics.  Not only did I need to write the code for things like hitbox detection and scoring, I had to read up on the equations for gravity and how to impliment those mathematical breadkdowns into ruby.  I still have more to learn, like adding a the means to log and track high scores using databases.</p>
+
+            <h4>Links</h4>
+            <a href="https://github.com/domenicbetters/Gluttony-of-Gore"><p>Git Hub Repo</p></a>
+            <a href="https://www.artstation.com/domenicbetters/albums/6208842"><p>Art Portfolio</p></a>
             
         </div>
         <div class="my-page">
@@ -57,7 +64,7 @@ minWidth: 200,
 maxWidth: 1200,
 minHeight: 266.666,
 maxHeight: 1600,
-
+disableFlipByClick: true,
 });
 
   pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
@@ -81,6 +88,7 @@ maxHeight: 1600,
 #book {
   box-shadow: #494029 5px 0px,  #494029 -5px 0px, #6f6446 10px 0px,  #6f6446 -10px 0px, #988c6c 15px 0px, #988c6c -15px 0px, #b4a98b -20px 0px, #b4a98b 20px 0px, #dbd2ba -25px 0px, #dbd2ba 25px 0px,#1d170a 30px 0px, #1d170a -30px 0px;
   border-radius: 1px;
+  border: solid 5px pink;
   overflow: hidden;
 }
 
@@ -139,7 +147,18 @@ h1 {
     align-items: start;
     justify-content: start;
     padding-left: 15px;
+    width: fit-content !important;
+    overflow-y: scroll;
   }
+
+  @media (max-width: 532.5px) {
+    .info-page {
+      width: 100%;
+    }
+  }
+
+
+
 
   
 /* 
