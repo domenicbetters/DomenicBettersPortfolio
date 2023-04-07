676e13ed3c049bccf5b90328b43bb73468be4c41
Domenic Betters <domenicbetters@yahoo.com>
2023-04-07 12:27:25 -0400

    worked on projects page and added my second project to the book

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view :key="componentKey" />
     <FooterBar class = "footer" />
   </div>
 </template>
@@ -12,10 +12,25 @@ import FooterBar from './components/FooterBar.vue'
 
 
 export default {
+  data() {
+    return {
+      componentKey: 0,
+    };
+  },
+  methods: {
+    forceRerender() {
+      this.componentKey += 1
+    }
+  },
   name: 'App',
   components: {
     FooterBar,
-  }
+  },
+  async mounted () {
+    window.addEventListener('resize', () => {
+   this.forceRerender();
+});
+  },
 }
 </script>
 
--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -2,6 +2,7 @@
   <div class = "project-body">
    <router-link to="/"><p class = "back-button menu-link">Home</p></router-link>
    <h1>The Projects of one Domenic Betters</h1>
+   <p class = "page-buttons"><i class="fa fa-angle-double-left" aria-hidden="true"></i>    |    <i  class="fa fa-angle-double-right" aria-hidden="true"></i></p>
     <div class="book-box">
       <div id="book">
         <div class="my-page" data-density="soft">
@@ -21,19 +22,25 @@
             <h4>Links</h4>
             <a href="https://github.com/domenicbetters/Gluttony-of-Gore"><p>Git Hub Repo</p></a>
             <a href="https://www.artstation.com/domenicbetters/albums/6208842"><p>Art Portfolio</p></a>
-            
-        </div>
-        <div class="my-page">
-            Page two
-        </div>
-        <div class="my-page">
-            Page three
-        </div>
-        <div class="my-page">
-            Page four
         </div>
         <div class="my-page" data-density="soft">
-            Last page
+            <h2>Antonios Pizzeria Website</h2>
+            <div class = "project-img" >
+              <iframe width="100%" height="100%" src="https://domenicbetters.github.io/AntoniosPizza" title="description" style="-webkit-transform:scale(1);-moz-transform-scale(1);"></iframe>
+             </div>
+        </div>
+        <div class="my-page info-page">
+            <h3>Antonios Pizzeria Website (continued...)</h3>
+            <h4>What is it?</h4>
+            <p>This is the website for our pizza shop, Antonio’s Pizzeria.  The site was built as a single page app using Vue.js along with several plug-ins such as bootstrap.  In addition to providing basic info about our shop, the menu is also configured to display full or vegan only items with the click of a button.  The menu is populated using SQL queries run directly against our POS database to make sure the info is always up to date and make life easier for me when it comes to site updates.   The SQL queries put the data into JSON format so Vue can display the info properly.  </p>
+
+            <h4>What did I learn?</h4>
+            <p>A lot.  This project helped me further my understanding of the Vue.js framework, including proper use of routers and configurations that make the app function more as a traditional website for internet deployment.  I was also able to learn a good deal about writing SQL queries that pull info from a SQL database and how to use that info in a dynamic rendering with in our menu.  I also got to dabble more in web animation techniques, which I would then carry over in a more advanced way to my next project...</p>
+
+            <h4>Links</h4>
+            <a href="https://github.com/domenicbetters/AntoniosPizza"><p>Git Hub Repo</p></a>
+            <a href="https://domenicbetters.github.io/AntoniosPizza/"><p>Full Site Link</p></a>
+            <a href="https://antoniospizzeria.hungerrush.com/"><p>Order here if you are hungry!</p></a>
         </div>
       </div>
     </div>
@@ -46,6 +53,13 @@
 
 import { PageFlip } from 'page-flip';
 
+var lengthy = window.innerWidth
+
+window.addEventListener('resize', () => {
+    lengthy = window.innerWidth
+    console.log(lengthy)
+});
+
 
 
   
@@ -55,6 +69,9 @@ import { PageFlip } from 'page-flip';
     
     },
 async mounted() {
+
+
+  if (lengthy >= 532) {
 
   const pageFlip = new PageFlip(document.getElementById('book'), {
 width: 600, // required parameter - base page width
@@ -66,8 +83,45 @@ minHeight: 266.666,
 maxHeight: 1600,
 disableFlipByClick: true,
 });
+  
 
   pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
+
+  document.querySelector(".fa-angle-double-left").addEventListener("click", () => {
+        pageFlip.flipPrev(); // Turn to the previous page (with animation)
+    });
+
+    document.querySelector(".fa-angle-double-right").addEventListener("click", () => {
+        pageFlip.flipNext(); // Turn to the next page (with animation)
+    });
+
+}
+else {
+  const pageFlip = new PageFlip(document.getElementById('book'), {
+width: 600, // required parameter - base page width
+height: 800, // required parameter - base page height
+size: "stretch",
+minWidth: 200,
+maxWidth: 1200,
+minHeight: 266.666,
+maxHeight: 1600,
+disableFlipByClick: true,
+useMouseEvents: false,
+});
+  
+
+  pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
+
+  document.querySelector(".fa-angle-double-left").addEventListener("click", () => {
+        pageFlip.turnToPrevPage(); // Turn to the previous page (with animation)
+    });
+
+    document.querySelector(".fa-angle-double-right").addEventListener("click", () => {
+        pageFlip.flipNext(); // Turn to the next page (with animation)
+    });
+}
+
+
 }
     
   }
@@ -88,7 +142,6 @@ disableFlipByClick: true,
 #book {
   box-shadow: #494029 5px 0px,  #494029 -5px 0px, #6f6446 10px 0px,  #6f6446 -10px 0px, #988c6c 15px 0px, #988c6c -15px 0px, #b4a98b -20px 0px, #b4a98b 20px 0px, #dbd2ba -25px 0px, #dbd2ba 25px 0px,#1d170a 30px 0px, #1d170a -30px 0px;
   border-radius: 1px;
-  border: solid 5px pink;
   overflow: hidden;
 }
 
@@ -130,8 +183,26 @@ a {
 .project-img iframe {
   transform: scale(2);
 }
+
+.page-buttons {
+  font-size: 2em;
+  color: #1d170aa4;
+  margin: 0;
+}
+.fa:hover{
+  color: #1d170a41
+}
+
+.fa:active {
+  color: #1d170a
+}
+
+
+
 img {
   width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 
 h1 {
@@ -156,6 +227,20 @@ h1 {
       width: 100%;
     }
   }
+
+::-webkit-scrollbar {
+  background: #1d170a4d;
+  width: 10px;
+  border-left: 1px solid #1d170a85;
+}
+
+::-webkit-scrollbar-thumb {
+    background: #1d170a85;
+    border-radius: 20px;
+;
+}
+
+
 
 
 
